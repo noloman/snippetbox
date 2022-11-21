93e0790079947aa68a9da1306bc8f1659023e94b
Manuel Lorenzo <manulorenzop@gmail.com>
2022-11-21 17:55:48 +0100

    add two new routes

--- main.go
+++ main.go
@@ -5,14 +5,23 @@ import (
 	"net/http"
 )
 
-
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox!"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
