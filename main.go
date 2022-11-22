e9d2e17f2696678b6cb7b444853ada7d6c9f777f
Manuel Lorenzo <manulorenzop@gmail.com>
2022-11-22 18:50:23 +0100

    add method not allowed error to snippet creation

--- main.go
+++ main.go
@@ -19,6 +19,11 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("Create a new snippet..."))
 }
 
