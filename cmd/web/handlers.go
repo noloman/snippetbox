8a584c865f5ab1a68bb77894c3e9ea7ad4fcd901
Manuel Lorenzo <manulorenzop@gmail.com>
2022-11-25 14:07:10 +0100

    define an application struct and pass it to the handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,7 +8,7 @@ import (
     "strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app application) home(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/" {
         http.NotFound(w, r)
         return
@@ -31,7 +31,7 @@ func home(w http.ResponseWriter, r *http.Request) {
     }
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app application) snippetView(w http.ResponseWriter, r *http.Request) {
     id, err := strconv.Atoi(r.URL.Query().Get("id"))
     if err != nil || id < 1 {
         http.NotFound(w, r)
@@ -41,7 +41,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app application) snippetCreate(w http.ResponseWriter, r *http.Request) {
     if r.Method != http.MethodPost {
         w.Header().Set("Allow", http.MethodPost)
         http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+type application struct {
+    errorLog log.Logger
+    infoLog log.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
@@ -14,12 +19,17 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+    app := application{
+        errorLog: *errorLog,
+        infoLog: *infoLog,
+    }
+
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	infoLog.Printf("Starting server on %s", *addr)
 	srv := http.Server{
