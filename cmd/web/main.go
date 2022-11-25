77fef43bbbaa3b928945c4476a1bfdc248285ad0
Manuel Lorenzo <manulorenzop@gmail.com>
2022-11-25 13:51:46 +0100

    create info and error loggers and use them in the server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,32 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "flag"
+	"flag"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-    addr := flag.String("addr", ":4000", "HTTP network address")
-    flag.Parse()
-    mux := http.NewServeMux()
-    fileServer := http.FileServer(http.Dir("./ui/static"))
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-    mux.HandleFunc("/", home)
-    mux.HandleFunc("/snippet/view", snippetView)
-    mux.HandleFunc("/snippet/create", snippetCreate)
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
-    log.Printf("Starting server on %s", *addr)
-    err := http.ListenAndServe(*addr, mux)
-    log.Fatal(err)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	infoLog.Printf("Starting server on %s", *addr)
+	srv := http.Server{
+        Addr: *addr,
+        ErrorLog: errorLog,
+        Handler: mux,
+    }
+    err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
