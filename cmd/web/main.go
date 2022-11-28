ef3aba700261e69002e9d3736cc935361039cba4
Manuel Lorenzo <manulorenzop@gmail.com>
2022-11-28 16:28:23 +0100

    extract ServeMux logic to its own file and method

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,18 +24,11 @@ func main() {
         infoLog: *infoLog,
     }
 
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	infoLog.Printf("Starting server on %s", *addr)
 	srv := http.Server{
         Addr: *addr,
         ErrorLog: errorLog,
-        Handler: mux,
+        Handler: app.routes(),
     }
     err := srv.ListenAndServe()
 	errorLog.Fatal(err)
