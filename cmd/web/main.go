5515b8bd1b1da292dde2155dfe0dc48c1cbe424c
Manuel Lorenzo <manulorenzop@gmail.com>
2022-11-30 16:03:57 +0100

    prepare DB connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,15 +16,23 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQ: data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-    app := application{
-        errorLog: *errorLog,
-        infoLog: *infoLog,
-    }
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
+	app := application{
+			errorLog: *errorLog,
+			infoLog: *infoLog,
+	}
 
 	infoLog.Printf("Starting server on %s", *addr)
 	srv := http.Server{
@@ -30,6 +40,17 @@ func main() {
         ErrorLog: errorLog,
         Handler: app.routes(),
     }
-    err := srv.ListenAndServe()
+  err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
