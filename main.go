e675f1d5a01390472442c8cde2c2fb2a6b13a29f
Manuel Lorenzo <manulorenzop@gmail.com>
2023-09-23 18:00:45 +0200

    create a minimum handler for "/"

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
