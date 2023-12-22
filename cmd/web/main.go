348fa9ebdd704014da6df68629f60c6c5ca38b78
Manuel Lorenzo <manulorenzop@gmail.com>
2023-12-22 23:58:48 +0100

    🔒️ add connection timeouts

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -117,6 +117,9 @@ func main() {
 		// could pass slog.LevelWarn as the final parameter.
 		ErrorLog:  slog.NewLogLogger(logger.Handler(), slog.LevelError),
 		TLSConfig: tlsConfig,
+		IdleTimeout: time.Minute,
+		ReadTimeout: 5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	logger.Info("starting server", "addr", *addr)
