4d40f2ffe50e65d79980f73a30f2860e76d73e3b
Manuel Lorenzo <manulorenzop@gmail.com>
2024-01-07 21:25:42 +0100

    🔒️ use context to check if user is authenticated in middleware

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -75,5 +75,9 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 }
 
 func (app *application) isAuthenticated(r *http.Request) bool {
-	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
+	isAuthenticated, ok := r.Context().Value(isAuthenticatedContextKey).(bool)
+	if !ok {
+		return false
+	}
+	return isAuthenticated
 }
--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
@@ -15,6 +16,37 @@ func noSurf(next http.Handler) http.Handler {
 		Secure:   true,
 	})
 	return csrfHandler
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Retrieve the authenticatedUserID value from the session using the
+		// GetInt() method. This will return the zero value for an int (0) if no
+		// "authenticatedUserID" value is in the session -- in which case we
+		// call the next handler in the chain as normal and return.
+		id := app.sessionManager.GetInt(r.Context(), "authenticatedUserID")
+		if id == 0 {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		// Otherwise, we check to see if a user with that ID exists in our database.
+		exists, err := app.userModel.Exists(id)
+		if err != nil {
+			app.serverError(w, r, err)
+			return
+		}
+
+		// If a matching user is found, we know that the request is
+		// coming from an authenticated user who exists in our database. We
+		// create a new copy of the request (with an isAuthenticatedContextKey
+		// value of true in the request context) and assign it to r.
+		if exists {
+			ctx := context.WithValue(r.Context(), isAuthenticatedContextKey, true)
+			r = r.WithContext(ctx)
+		}
+		next.ServeHTTP(w, r)
+	})
 }
 
 func secureHeaders(next http.Handler) http.Handler {
