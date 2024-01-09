237b7aa6304ca256f15e92483f1ff14ab17fa596
Manuel Lorenzo <manulorenzop@gmail.com>
2024-01-09 15:27:32 +0100

    ⚡️ embed static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
-	"net/http"
+	"github.com/noloman/snippetbox/ui"
 )
 
 func (app *application) routes() http.Handler {
@@ -16,8 +18,17 @@ func (app *application) routes() http.Handler {
 		app.notFound(w)
 	})
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	// Take the ui.Files embedded filesystem and convert it to a http.FS type so
+	// that it satisfies the http.FileSystem interface. We then pass that to the
+	// http.FileServer() function to create the file server HTTP handler.
+	fileServer := http.FileServer(http.FS(ui.Files))
+	// Our static files are contained in the "static" folder of the ui.Files
+	// embedded filesystem. So, for example, our CSS stylesheet is located at
+	// "static/css/main.css". This means that we no longer need to strip the
+	// prefix from the request URL -- any requests that start with /static/ can
+	// just be passed directly to the file server and the corresponding static
+	// file will be served (so long as it exists).
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
 	// Unprotected application routes using the "dynamic" middleware chain.
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
